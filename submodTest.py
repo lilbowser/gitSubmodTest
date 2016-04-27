c09646a9c33444fc693cc7f3a20fdf0ad8928841
lilbowser <ham@x303.com>
2016-04-27 10:24:55 -0400

    Testing global .gitignore

--- submodTest.py
+++ submodTest.py
@@ -1,7 +1,12 @@
 """
 A test project for experimenting with git submodule and subtree
-"""
 
+If we want the main project to be in a folder along side modules:
+import sys
+sys.path.append("..") # Adds higher directory to python modules path.
+"""
+from modules.module1 import mod
 
 if __name__ == '__main__':
+    mod.pr()
     print("Submod test!")
